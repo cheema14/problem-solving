5b368e0089b92db16c8411e5ff3f36c22be45111
ali hassan <ali.hassan@pitb.gov.pk>
2023-09-28 16:16:49 +0500

    problem:defanging an IP address

---
+++ defangIp.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function defangIPaddr($address) {
+        
+    $validIpAddr = substr_count($address,".");
+    
+    if($validIpAddr < 3 || $validIpAddr > 3){   
+      echo "invalid format:<strong>".$address.'</strong>';
+    }
+    else{
+      echo str_replace(".","[.]",$address);
+    }
+
+
+}
+
+
+defangIPaddr("8.8.8.8.8");
